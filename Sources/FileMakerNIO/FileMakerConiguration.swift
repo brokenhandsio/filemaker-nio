808ea1e54655dedf5985293ddc86d7a1d66383b2
Tim <0xtimc@gmail.com>
2020-04-14 17:06:45 +0100

    Make config init public

--- Sources/FileMakerNIO/FileMakerConiguration.swift
+++ Sources/FileMakerNIO/FileMakerConiguration.swift
@@ -3,4 +3,11 @@ public struct FileMakerConfiguration {
     public let databaseName: String
     public let username: String
     public let password: String
+    
+    public init(hostname: String, databaseName: String, username: String, password: String) {
+        self.hostname = hostname
+        self.databaseName = databaseName
+        self.username = username
+        self.password = password
+    }
 }

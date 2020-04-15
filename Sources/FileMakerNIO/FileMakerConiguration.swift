861796219b80f06abb8a7eb8c83ef5de7414572d
Tyrfing Mjølner <4lphamal3@gmail.com>
2020-04-15 18:15:08 +0200

    Update FileMakerConiguration.swift

--- Sources/FileMakerNIO/FileMakerConiguration.swift
+++ Sources/FileMakerNIO/FileMakerConiguration.swift
@@ -3,11 +3,13 @@ public struct FileMakerConfiguration {
     public let databaseName: String
     public let username: String
     public let password: String
+    public let queryPath: String
     
-    public init(hostname: String, databaseName: String, username: String, password: String) {
+    public init(hostname: String, databaseName: String, username: String, password: String, queryPath: String = "" ) {
         self.hostname = hostname
         self.databaseName = databaseName
         self.username = username
         self.password = password
+        self.queryPath = queryPath
     }
 }

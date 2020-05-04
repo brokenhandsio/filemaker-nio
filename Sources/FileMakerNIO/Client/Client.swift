50dcb8d9ef887af74f1685910ecab071f92941f0
Tim <0xtimc@gmail.com>
2020-05-04 16:23:58 +0100

    Log full request in client

--- Sources/FileMakerNIO/Client/Client.swift
+++ Sources/FileMakerNIO/Client/Client.swift
@@ -1,9 +1,10 @@
 import AsyncHTTPClient
 import NIO
 import NIOHTTP1
+import Logging
 
 public protocol Client {
     var eventLoopGroup: EventLoopGroup { get }
-    func sendRequest<T: Encodable>(to url: String, method: HTTPMethod, data: T, sessionToken: String?) -> EventLoopFuture<HTTPClient.Response>
+    func sendRequest<T: Encodable>(to url: String, method: HTTPMethod, data: T, sessionToken: String?, logger: Logger) -> EventLoopFuture<HTTPClient.Response>
     func sendRequest(to url: String, method: HTTPMethod, sessionToken: String?) -> EventLoopFuture<HTTPClient.Response>
 }
--- Sources/FileMakerNIO/Client/HTTPClient+Requester.swift
+++ Sources/FileMakerNIO/Client/HTTPClient+Requester.swift
@@ -2,6 +2,7 @@ import AsyncHTTPClient
 import NIOHTTP1
 import NIO
 import Foundation
+import Logging
 
 extension HTTPClient: Client {
     
@@ -20,7 +21,7 @@ extension HTTPClient: Client {
         return self.execute(request: request)
     }
     
-    public func sendRequest<T>(to url: String, method: HTTPMethod, data: T, sessionToken: String?) -> EventLoopFuture<Response> where T : Encodable {
+    public func sendRequest<T>(to url: String, method: HTTPMethod, data: T, sessionToken: String?, logger: Logger) -> EventLoopFuture<Response> where T : Encodable {
         var headers = HTTPHeaders()
         headers.add(name: "content-type", value: "application/json")
         if let token = sessionToken {
@@ -30,6 +31,7 @@ extension HTTPClient: Client {
         do {
             let body = try JSONEncoder().encodeAsByteBuffer(data, allocator: ByteBufferAllocator())
             request = try HTTPClient.Request(url: url, method: method, headers: headers, body: Body.byteBuffer(body))
+            logger.trace("Sending request \(request)")
         } catch {
             return self.eventLoopGroup.next().makeFailedFuture(error)
         }

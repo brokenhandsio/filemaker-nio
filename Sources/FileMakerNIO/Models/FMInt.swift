ed3b8ca2f34a93e7ec6ec2aeb18ef4308ae47f2c
Tim <0xtimc@gmail.com>
2020-05-05 17:38:42 +0100

    Introduce property wrappers for Int to try and stop decoding breaking

---
+++ Sources/FileMakerNIO/Models/FMInt.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+@propertyWrapper
+public final class FMInt: Codable {
+    
+    public var value: Int?
+    
+    public init(wrappedValue value: Int?) {
+        self.value = value
+    }
+    
+    public var wrappedValue: Int? {
+        get { value }
+        set { value = newValue }
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let intValue = try? container.decode(Int.self) {
+            self.wrappedValue = intValue
+        } else {
+            let stringValue = try container.decode(String.self)
+            if stringValue == "" {
+                self.wrappedValue = nil
+            } else {
+                throw FileMakerNIOError(message: "Invalid type when decoding FMInt")
+            }
+        }
+    }
+    
+//    public func encode(to encoder: Encoder) throws {
+//        var container = encoder.singleValueContainer()
+//        try container.encode(self.wrappedValue)
+//    }
+//
+//    public func decode(from decoder: Decoder) throws {
+//        let container = try decoder.singleValueContainer()
+//        self.wrappedValue = try container.decode(Int.self)
+//    }
+}
+
